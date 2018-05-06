8db9e4f6cdf719aaad79476d2882118da2a3ff65
s0ph0s-2 <alexander.g.langley@gmail.com>
2018-05-06 04:31:31 -0400

    Setuptools is more difficult than I thought

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     url='https://github.com/s0ph0s-2/Gelo',
     author='s0ph0s-2',
     license='GPL v2',
-    packages=find_packages(),
+    packages=['gelo'],
+    package_dir={'gelo': 'gelo'},
+    package_data={'gelo': ['plugins/*.gelo-plugin']},
     install_requires=[
         'yapsy',
         'requests',
