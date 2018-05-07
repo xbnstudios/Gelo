76153336ff7a64dfb1db2de747e9d39dc74d2c28
s0ph0s-2 <alexander.g.langley@gmail.com>
2018-05-07 11:14:43 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gelo',
-    version='1.1.2',
+    version='1.1.3',
     description='podcast chapter metadata gathering tool for content creators',
     url='https://github.com/s0ph0s-2/Gelo',
     author='s0ph0s-2',
