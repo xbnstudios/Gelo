a007b0c26a68edd04fc6dd264e779b93ceb5ee0f
s0ph0s-dog <awoo@s0ph0s.dog>
2026-07-24 22:53:28 -0400

    no nix in selfci

--- .config/selfci/ci.sh
+++ .config/selfci/ci.sh
@@ -3,14 +3,17 @@ set -eou pipefail
 
 case "$SELFCI_JOB_NAME" in
   main)
+    uv sync
     selfci job start "lint"
     selfci job start "test"
     ;;
   test)
-    nix build -L .#ci.test
+    #nix build -L .#ci.test
+		uv run pytest
     ;;
   lint)
-    nix build -L .#ci.lint
+    #nix build -L .#ci.lint
+		nix-shell -p ty --command "ty check"
     ;;
   *)
     echo "Unknown job: $SELFCI_JOB_NAME"
