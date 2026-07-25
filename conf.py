5f4e45cc7d9261f12e477a2ebc3e2657c3c4c79d
s0ph0s-dog <awoo@s0ph0s.dog>
2026-07-24 23:05:18 -0400

    Bump version (oops)

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = "2019, s0ph0s"
 author = "s0ph0s"
 
 # The short X.Y version
-version = "6.0"
+version = "7.0"
 # The full version, including alpha/beta/rc tags
-release = "v6.0.1"
+release = "v7.0.1"
 
 
 # -- General configuration ---------------------------------------------------
